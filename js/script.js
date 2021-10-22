7f1b9dec14acc26cd26a61d2b996c7363b2e8fe8
jelsey1967 <jelsey1967@gmail.com>
2021-10-22 12:45:59 +0700

    add adaptiv slider

--- js/script.js
+++ js/script.js
@@ -7,10 +7,24 @@ const swiper = new Swiper('.slider-bottle', {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
+
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		500: {
+			loop: true,
+			slidesPerView: 2,
+		},
+		768: {
+			loop: true,
+			slidesPerView: 3,
+		},
+	},
 });
 
 const swiperFollow = new Swiper('.slider-follow', {
 	slidesPerView: 'auto',
-	loopedSlides: true,
+	loopedSlides: false,
 	spaceBetween: 9,
 });
